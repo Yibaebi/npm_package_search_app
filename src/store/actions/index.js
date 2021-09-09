d7bc91ea3b94045ec594a1ca619df3a9ea5afee8
Yibaebi <elliot.yibaebi@yahoo.com>
2021-09-09 23:12:33 +0100

    NEW: wire-up application action creators

---
+++ src/store/actions/index.js
@@ -0,0 +1,15 @@
+import { ActionTypes } from '../action-types'
+
+export interface SearchRepositoriesAction {
+  type: ActionTypes.SEARCH_REPOSITORIES;
+}
+export interface SearchRepositoriesSuccessAction {
+  type: ActionTypes.SEARCH_REPOSITORIES_SUCCESS;
+  payload: string[];
+}
+export interface SearchRepositoriesErrorAction {
+  type: ActionTypes.SEARCH_REPOSITORIES_ERROR;
+  payload: string;
+}
+
+export type Action = SearchRepositoriesAction | SearchRepositoriesSuccessAction | SearchRepositoriesErrorAction

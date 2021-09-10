91cd125e9e295b61be7df48081b2f99ee4bc543b
Yibaebi <elliot.yibaebi@yahoo.com>
2021-09-10 09:45:03 +0100

    CHORE: wire up reducers and store

--- src/store/reducers/repositoriesReducer.ts
+++ src/store/reducers/repositoriesReducer.ts
@@ -1,5 +1,5 @@
 import { ActionTypes } from '../action-types'
-import { Action } from '../actions'
+import { SearchAction } from '../actions/index'
 
 interface RepositoriesState {
   loading: boolean
@@ -7,14 +7,17 @@ interface RepositoriesState {
   data: string[]
 }
 
-export const reducer = (state: RepositoriesState, action: Action) => {
+export const repositoriesReducer = (state: RepositoriesState, action: SearchAction) => {
   switch (action.type) {
     case ActionTypes.SEARCH_REPOSITORIES:
       return { loading: true, error: null, data: [] }
+
     case ActionTypes.SEARCH_REPOSITORIES_SUCCESS:
       return { loading: false, error: null, data: action.payload }
+
     case ActionTypes.SEARCH_REPOSITORIES_ERROR:
       return { loading: false, error: action.payload, data: [] }
+
     default:
       return state
   }

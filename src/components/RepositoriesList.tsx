259fe1c402e3d20424acffd54f6866a779586f4c
Yibaebi <elliot.yibaebi@yahoo.com>
2021-09-12 23:34:51 +0100

    NEW: create a tag component for displaying keywords of each search results.

--- src/components/RepositoriesList.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const RepositoriesList: React.FC = () => {
-  return <div>Repositories list</div>
-}
-
-export default RepositoriesList
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export * from './search/search'
+export * from './searchResults/searchResultsList'
+export * from './spinner/Spinner'
 export * from './tag/Tag'

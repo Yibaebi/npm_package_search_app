aa51a347cb9ef9623d3a513fbfdb645031419554
Yibaebi <elliot.yibaebi@yahoo.com>
2021-09-12 23:28:51 +0100

    UPDATE: update button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,17 +1,26 @@
 import React from 'react'
-import { searchIcon } from '../../assets'
+import { Spinner } from '..'
 import './Button.css'
 
 interface ButtonProps {
   label: string
+  icon?: () => JSX.Element
   className?: string
+  loading: boolean
+  disabled: boolean
 }
 
-const Button: React.FC<ButtonProps> = ({ label = 'Label', className = '' }) => {
+const Button: React.FC<ButtonProps> = ({ icon, label = 'Label', className = '', loading, disabled }) => {
   return (
-    <button className={`nps-btn ${className}`}>
-      {searchIcon()}
-      {label}
+    <button className={`nps-btn ${className}  ${loading ? 'loading' : ''}`} disabled={disabled}>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <React.Fragment>
+          {icon && icon()}
+          {label}
+        </React.Fragment>
+      )}
     </button>
   )
 }

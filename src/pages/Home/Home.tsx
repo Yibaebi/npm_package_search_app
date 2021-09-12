e75c6eee373413806bd7eac8aea06d9036e989fd
Yibaebi <elliot.yibaebi@yahoo.com>
2021-09-12 23:37:11 +0100

    UPDATE: complete UI of home page with search component.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
-import RepositoriesList from '../../components/RepositoriesList'
+import { Search } from '../../components'
+import './Home.css'
 
 const Home: React.FC = () => {
   return (
-    <main>
-      <h1>Click here to search for a package</h1>
-      <RepositoriesList />
+    <main className="nps-home--main">
+      <section className="nps-search--container">
+        <div className="logo--container">
+          <i className="fab fa-npm"></i>
+        </div>
+        <h2 className="">
+          The worlds's largest database of third party packages for your web application. Contains over 1.3 million
+          packages to choose from.
+        </h2>
+        <Search page="home" />
+      </section>
     </main>
   )
 }
